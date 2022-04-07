21a40dc6140cc11ccff63f68cb2e7941b0d692bc
matiss-norenbergs <97153551+matiss-norenbergs@users.noreply.github.com>
2022-04-07 21:25:23 +0300

    Labojumu prieksh game API

--- server/models/gameModel.js
+++ server/models/gameModel.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const reviewSchema = Schema({
+    reviewText: { type: String },
+    reviewAuthor: { type: String },
+    reviewAuthorId: { type: String }
+}, { _id : true });
+
 const gameSchema = Schema(
     {
         title: {
@@ -11,14 +17,46 @@ const gameSchema = Schema(
             type: String,
             required: [true, "Provide a link for the games cover picture"]
         },
-        summary : {
+        summary: {
             type: String,
             required: [true, "Provide a summary for the game"]
         },
+        tags: [
+            {
+                type: String
+            }
+        ],
+        rating1: {
+            type: Number,
+            default: 0
+        },
+        rating2: {
+            type: Number,
+            default: 0
+        },
+        rating3: {
+            type: Number,
+            default: 0
+        },
+        rating4: {
+            type: Number,
+            default: 0
+        },
+        rating5: {
+            type: Number,
+            default: 1
+        },
+        reviews: [
+            reviewSchema
+        ],
+        public: {
+            type: Boolean,
+            default: false
+        }
     },
     {
         timestamps: true
     }
-)
+);
 
 module.exports = mongoose.model("Game", gameSchema)
