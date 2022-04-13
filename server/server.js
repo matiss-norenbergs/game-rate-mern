b0343d89970252add571b92d9f2baf534a4196c0
matiss-norenbergs <97153551+matiss-norenbergs@users.noreply.github.com>
2022-04-13 18:46:01 +0300

    Deploy

--- server/server.js
+++ server/server.js
@@ -18,9 +18,11 @@ app.use("/api/tags", require("./routes/tagRoutes"));
 
 //Serve front end  "client side"
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static("../client/build"));
 
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
+    });
 }else{
     app.get("/", (req, res) => res.send("Please set to production"));
 }
